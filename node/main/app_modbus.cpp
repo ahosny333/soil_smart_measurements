8045bb86bb6617177ede6f9c3740959af9c44b7e
ahmed hosny <ahmed.hosny@the-diy-life.co>
2024-02-26 13:54:22 +0200

    adjust adresses for npk sensor reading values

--- node/main/app_modbus.cpp
+++ node/main/app_modbus.cpp
@@ -4,6 +4,12 @@
 
 ModbusRTU mb;
 uint16_t regs[5];
+
+#ifdef NPK_SENSOR
+  uint16_t sn_N,sn_P,sn_K;
+#endif
+
+
 bool HRead(Modbus::ResultCode event, uint16_t transactionId, void* data) {
   Serial.print("Request result: 0x");
   Serial.print(event, HEX);
@@ -19,17 +25,22 @@ void modbus_init()
 
 void read_sensors_values()
 {
+  uint8_t addr;
 
   #ifdef NPK_SENSOR
+    addr = 0x001E;
     if (!mb.slave()) {    // Check if no transaction in progress
-    mb.readHreg(NPK_SENSOR_ADDR, 0x00, regs, 3, HRead); // Send Read Hreg from Modbus Server
+    mb.readHreg(NPK_SENSOR_ADDR, addr, regs, 3, HRead); // Send Read Hreg from Modbus Server
     while(mb.slave()) { // Check if transaction is active
       mb.task();
       delay(10);
     }
+    sn_N = float(regs[0]);
+    sn_P = float(regs[1]);
+    sn_K = float(regs[2]);
     Serial.println(regs[0]);
     Serial.println(regs[1]);
-    Serial.println(regs[3]);
+    Serial.println(regs[2]);
     }
     delay(1000);
   #endif
