f7769c378ee3264af84ee1bfa98bd1feb865db23
unknown <Ahmed.Yasser@tatware.com>
2024-03-06 19:13:35 +0200

    All drivers were written to be measured against real sensors

--- node/main/app_modbus.cpp
+++ node/main/app_modbus.cpp
@@ -13,10 +13,6 @@ uint16_t NPK_N, NPK_P, NPK_K;
 bool NPK_ok = false;
 #endif
 
-#ifdef EC_SENSOR
-uint16_t EC_ec, EC_tds;
-#endif
-
 #ifdef MT_SENSOR
 uint16_t MT_M, MT_T;
 bool MT_ok = false;
@@ -25,6 +21,16 @@ bool MT_ok = false;
 #ifdef T_M_EC_S_SENSOR
 uint16_t T_M_EC_S_T, T_M_EC_S_M, T_M_EC_S_EC, T_M_EC_S_S;
 bool T_M_EC_S_ok = false;
+#endif
+
+#ifdef EC_SENSOR
+uint16_t EC_T, EC_M;
+bool EC_ok = false;
+#endif
+
+#ifdef PH_SENSOR
+uint16_t PH_T, PH_M;
+bool PH_ok = false;
 #endif
 
 bool HRead(Modbus::ResultCode event, uint16_t transactionId, void *data)
@@ -49,62 +55,10 @@ void read_sensors_values()
 {
   uint8_t addr;
 
-  Serial.println("------------------------------------------------");
-  Serial.println("T_M_EC_S_SENSOR");
+  //////////////////////////////////////////////////////
 
-#ifdef T_M_EC_S_SENSOR
-  retry_counter = 0;
-  addr = T_M_EC_S_SENSOR_START_ADD;
-  mb_success = false;
-  while (mb_success == false && retry_counter < max_retries)
-  {
-    // Check if no transaction in progress
-    mb.readHreg(T_M_EC_S_SENSOR_ADDR, addr, regs, 4, HRead); // Send Read Hreg from Modbus Server
-    while (mb.slave())
-    { // Check if transaction is active
-      mb.task();
-      delay(10);
-    }
-
-    delay(1000);
-    if (mb_success == true)
-    {
-      T_M_EC_S_T = float(regs[0]);
-      T_M_EC_S_M = float(regs[1]);
-      T_M_EC_S_EC = float(regs[2]);
-      T_M_EC_S_S = float(regs[3]);
-    }
-    else
-    {
-      T_M_EC_S_T = 0;
-      T_M_EC_S_M = 0;
-      T_M_EC_S_EC = 0;
-      T_M_EC_S_S = 0;
-    }
-
-    Serial.println(T_M_EC_S_T);
-    Serial.println(T_M_EC_S_M);
-    Serial.println(T_M_EC_S_EC);
-    Serial.println(T_M_EC_S_S);
-
-    delay(1000);
-    retry_counter++;
-  }
-
-  if (mb_success == false)
-  {
-    T_M_EC_S_ok = false;
-  }
-  else
-  {
-    T_M_EC_S_ok = true;
-  }
-#endif
   Serial.println("------------------------------------------------");
   Serial.println("NPK_SENSOR");
-
-  //////////////////////
-
 #ifdef NPK_SENSOR
   retry_counter = 0;
   addr = NPK_SENSOR_START_ADD;
@@ -144,18 +98,17 @@ void read_sensors_values()
 
   if (mb_success == false)
   {
-    T_M_EC_S_ok = false;
+    NPK_ok = false;
   }
   else
   {
-    T_M_EC_S_ok = true;
+    NPK_ok = true;
   }
 #endif
+  ////////////////////////////////////////////////////////
+
   Serial.println("------------------------------------------------");
   Serial.println("MT_SENSOR");
-
-  //////////////////////
-
 #ifdef MT_SENSOR
   retry_counter = 0;
   addr = MT_SENSOR_START_ADD;
@@ -192,6 +145,54 @@ void read_sensors_values()
 
   if (mb_success == false)
   {
+    MT_ok = false;
+  }
+  else
+  {
+    MT_ok = true;
+  }
+#endif
+
+  ////////////////////////////////////////////////////////
+  Serial.println("------------------------------------------------");
+  Serial.println("T_M_EC_S_SENSOR");
+#ifdef T_M_EC_S_SENSOR
+  retry_counter = 0;
+  addr = T_M_EC_S_SENSOR_START_ADD;
+  mb_success = false;
+  while (mb_success == false && retry_counter < max_retries)
+  {
+    // Check if no transaction in progress
+    mb.readHreg(T_M_EC_S_SENSOR_ADDR, addr, regs, 4, HRead); // Send Read Hreg from Modbus Server
+    while (mb.slave())
+    { // Check if transaction is active
+      mb.task();
+      delay(10);
+    }
+    delay(1000);
+    if (mb_success == true)
+    {
+      T_M_EC_S_T = float(regs[0]);
+      T_M_EC_S_M = float(regs[1]);
+      T_M_EC_S_EC = float(regs[2]);
+      T_M_EC_S_S = float(regs[3]);
+    }
+    else
+    {
+      T_M_EC_S_T = 0;
+      T_M_EC_S_M = 0;
+      T_M_EC_S_EC = 0;
+      T_M_EC_S_S = 0;
+    }
+    Serial.println(T_M_EC_S_T);
+    Serial.println(T_M_EC_S_M);
+    Serial.println(T_M_EC_S_EC);
+    Serial.println(T_M_EC_S_S);
+    delay(1000);
+    retry_counter++;
+  }
+  if (mb_success == false)
+  {
     T_M_EC_S_ok = false;
   }
   else
@@ -199,13 +200,104 @@ void read_sensors_values()
     T_M_EC_S_ok = true;
   }
 #endif
-}
 
+  ////////////////////////////////////////////////////////
+  Serial.println("------------------------------------------------");
+  Serial.println("EC_SENSOR");
+#ifdef EC_SENSOR
+  retry_counter = 0;
+  addr = EC_SENSOR_START_ADD;
+  mb_success = false;
+  while (mb_success == false && retry_counter < max_retries)
+  {
+    // Check if no transaction in progress
+    mb.readHreg(EC_SENSOR_ADDR, addr, regs, 2, HRead); // Send Read Hreg from Modbus Server
+    while (mb.slave())
+    { // Check if transaction is active
+      mb.task();
+      delay(10);
+    }
+
+    delay(1000);
+    if (mb_success == true)
+    {
+      EC_T = float(regs[0]);
+      EC_M = float(regs[1]);
+    }
+    else
+    {
+      EC_T = 0;
+      EC_M = 0;
+    }
+
+    Serial.println(EC_T);
+    Serial.println(EC_M);
+
+    delay(1000);
+    retry_counter++;
+  }
+
+  if (mb_success == false)
+  {
+    EC_ok = false;
+  }
+  else
+  {
+    EC_ok = true;
+  }
+#endif
+
+  ////////////////////////////////////////////////////////
+  Serial.println("------------------------------------------------");
+  Serial.println("PH_SENSOR");
+#ifdef PH_SENSOR
+  retry_counter = 0;
+  addr = PH_SENSOR_START_ADD;
+  mb_success = false;
+  while (mb_success == false && retry_counter < max_retries)
+  {
+    // Check if no transaction in progress
+    mb.readHreg(PH_SENSOR_ADDR, addr, regs, 2, HRead); // Send Read Hreg from Modbus Server
+    while (mb.slave())
+    { // Check if transaction is active
+      mb.task();
+      delay(10);
+    }
+
+    delay(1000);
+    if (mb_success == true)
+    {
+      PH_T = float(regs[0]);
+      PH_M = float(regs[1]);
+    }
+    else
+    {
+      PH_T = 0;
+      PH_M = 0;
+    }
+
+    Serial.println(PH_T);
+    Serial.println(PH_M);
+
+    delay(1000);
+    retry_counter++;
+  }
+
+  if (mb_success == false)
+  {
+    PH_ok = false;
+  }
+  else
+  {
+    PH_ok = true;
+  }
+#endif
+}
 void data_serializer()
 {
 
   // Allocate the memory for the JSON document
-  StaticJsonBuffer<300> jsonBuffer;
+  StaticJsonBuffer<450> jsonBuffer;
 
   // Create the root JSON object
   JsonObject &root = jsonBuffer.createObject();
@@ -217,23 +309,50 @@ void data_serializer()
   // Create the 'reading' array
   JsonArray &reading = root.createNestedArray("r");
 
-// Add sensor readings to the 'reading' array
+  // Add sensor readings to the 'reading' array
+
+/////////////////////////////////////////////////
+// NPK_SENSOR
 #ifdef NPK_SENSOR
-  JsonObject &npkObj = reading.createNestedObject();
-  JsonArray &npkArray = npkObj.createNestedArray("npk");
-  npkArray.add(NPK_N);
-  npkArray.add(NPK_P);
-  npkArray.add(NPK_K);
+  if (NPK_ok)
+  {
+    JsonObject &NPK_Obj = reading.createNestedObject();
+    JsonArray &NPK_Array = NPK_Obj.createNestedArray("NPK");
+    NPK_Array.add(NPK_N);
+    NPK_Array.add(NPK_P);
+    NPK_Array.add(NPK_K);
+  }
+  else
+  {
+    JsonObject &NPK_Obj = reading.createNestedObject();
+    JsonArray &NPK_Array = NPK_Obj.createNestedArray("NPK");
+    NPK_Array.add("0");
+    NPK_Array.add("0");
+    NPK_Array.add("0");
+  }
 #endif
 
-#ifdef EC_SENSOR
-  JsonObject &ecObj = reading.createNestedObject();
-  JsonArray &ecArray = ecObj.createNestedArray("ec");
-  ecArray.add(EC_ec);
-  ecArray.add(EC_tds);
-  Serial.println("ec sensor");
+/////////////////////////////////////////////////
+// MT_SENSOR
+#ifdef MT_SENSOR
+  if (MT_ok)
+  {
+    JsonObject &MT_Obj = reading.createNestedObject();
+    JsonArray &MT_Array = MT_Obj.createNestedArray("MT");
+    MT_Array.add(MT_T);
+    MT_Array.add(MT_M);
+  }
+  else
+  {
+    JsonObject &MT_Obj = reading.createNestedObject();
+    JsonArray &MT_Array = MT_Obj.createNestedArray("MT");
+    MT_Array.add("0");
+    MT_Array.add("0");
+  }
 #endif
 
+/////////////////////////////////////////////////
+// T_M_EC_S_SENSOR
 #ifdef T_M_EC_S_SENSOR
   if (T_M_EC_S_ok)
   {
@@ -254,6 +373,46 @@ void data_serializer()
     T_M_EC_S_Array.add("0");
   }
 #endif
+
+/////////////////////////////////////////////////
+// EC_SENSOR
+#ifdef EC_SENSOR
+  if (EC_ok)
+  {
+    JsonObject &EC_Obj = reading.createNestedObject();
+    JsonArray &EC_Array = EC_Obj.createNestedArray("EC");
+    EC_Array.add(EC_T);
+    EC_Array.add(EC_M);
+  }
+  else
+  {
+    JsonObject &EC_Obj = reading.createNestedObject();
+    JsonArray &EC_Array = EC_Obj.createNestedArray("EC");
+    EC_Array.add("0");
+    EC_Array.add("0");
+  }
+#endif
+
+/////////////////////////////////////////////////
+// PH_SENSOR
+#ifdef PH_SENSOR
+  if (PH_ok)
+  {
+    JsonObject &PH_Obj = reading.createNestedObject();
+    JsonArray &PH_Array = PH_Obj.createNestedArray("PH");
+    PH_Array.add(PH_T);
+    PH_Array.add(PH_M);
+  }
+  else
+  {
+    JsonObject &PH_Obj = reading.createNestedObject();
+    JsonArray &PH_Array = PH_Obj.createNestedArray("PH");
+    PH_Array.add("0");
+    PH_Array.add("0");
+  }
+#endif
+
+
 
   // Serialize the JSON document to a string
   jsonStr.clear();
--- node/main/main.h
+++ node/main/main.h
@@ -26,36 +26,69 @@
 #define DEBUG_PRINTLNDEC(x)
 #endif
 
-// modbus settings
-#define max_retries 2 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Number of Retry Reading
+#define max_retries 1 //////////////////////////////////////////////
+//##################################################################
+// Sensor Address 
+#define NPK_SENSOR_ADDR 0x01 ////////////////////TRUE
+#define MT_SENSOR_ADDR 0x03 /////////////////////TRUE
+#define T_M_EC_S_SENSOR_ADDR 0x05 ///////////////TRUE
 
-#define NPK_SENSOR_ADDR 0x01 ///////////////////////////////////////////////done
-#define EC_SENSOR_ADDR 0x02
-#define MT_SENSOR_ADDR 0x03///////////////////////////////////////////////////////////////////////////////////////true
+#define EC_SENSOR_ADDR 0x02 /////////////////////TRUE
 #define PH_SENSOR_ADDR 0x04
 
-#define T_M_EC_S_SENSOR_ADDR 0x05 // TRUE/////////////////////////////////////////////////////
+//####################################################################
+// Enable Sensor 
+#define NPK_SENSOR 1 ////////////////////////////done
+#define MT_SENSOR 1 /////////////////////////////done
+#define T_M_EC_S_SENSOR 1 ///////////////////////done
 
+#define EC_SENSOR 1
+#define PH_SENSOR 1
 
-#define NPK_SENSOR 1 ////////////////////////////////////////////////done
-// #define EC_SENSOR
-#define MT_SENSOR 1 /////////////////////////////////////done
-// #define PH_SENSOR
-#define T_M_EC_S_SENSOR 1 // to set slave id use address 254 and start address 201 in qmodmaster fc = 0x06 then write the required id to 5
+// to set slave id use address 254 and start address 201 in qmodmaster fc = 0x06 then write the required id to 5
 
-#ifdef NPK_SENSOR////////////////////////////////////////////////////////////////////done
+//####################################################################
+// Register 
+
+#ifdef NPK_SENSOR//////////////////////////////////done
 #define NPK_SENSOR_START_ADD 0x001E 
 #endif
 
-#ifdef  MT_SENSOR
+#ifdef  MT_SENSOR//////////////////////////////////done
 #define MT_SENSOR_START_ADD 0x0000// CHANEABLE
 #endif
 
-
-
-#ifdef T_M_EC_S_SENSOR////////////////////////////////////////
+#ifdef T_M_EC_S_SENSOR/////////////////////////////done
 #define T_M_EC_S_SENSOR_START_ADD 0x0000
 #endif
+
+//////////////////////////////////////////////////////////
+
+#ifdef  EC_SENSOR//////////////////////////////////done
+#define EC_SENSOR_START_ADD 0x0000
+#endif
+
+#ifdef  PH_SENSOR//////////////////////////////////done
+#define PH_SENSOR_START_ADD 0x0000
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #define uS_TO_S_FACTOR 1000000 /* Conversion factor for micro seconds to seconds */
 #define INITIAL_AP_TIME 60000
